964938c42057160c58d9f57443fe52a32a7d2386
Alvar <alvarosg7@gmail.com>
2023-11-12 16:42:19 -0600

    fixed bug that was adding more than 1 subject header at a time. Added comments on how to set thresholds

--- health_check_improved.py
+++ health_check_improved.py
@@ -11,29 +11,26 @@ import logging
 
 logging.basicConfig(level=logging.DEBUG, format='%(asctime)s - %(levelname)s - %(message)s')
 
-#Email variables
-
-message = EmailMessage()
-gmail_user = ''     #enter the email account to send emails from
-message['From'] = gmail_user
-message['To'] = ''  #enter your the recepient's email account
-gmail_password = '' #enter your password or
-email__body = "Please check your system and resolve the issue as soon as possible."
-message.set_content(email__body)
-subject = ''
-
-
-
 #Set the thresholds for CPU,disk and virtual memory usage.
 
-cpu_threshold = 80
-disk_threshold = 45831193600
-memory_threshold = 524288000
+cpu_threshold = 80     #set your cpu usage threshold in percentage
+disk_threshold = 45831193600  # set your disk usage threshold in KB
+memory_threshold = 11524288000 # set your virtual memory threshold in KB
 
 #Function to generate email
 
 def send_email(subject):
+
+    #set email variables
+    message = EmailMessage()
+    gmail_user = ''     #enter the email account to send emails from
+    message['From'] = gmail_user
+    message['To'] = ''  #enter your the recepient's email account
+    gmail_password = '' #enter your password or app password in case you have 2FA activated 
+    email__body = "Please check your system and resolve the issue as soon as possible."
+    message.set_content(email__body)
     message['subject'] = subject
+   
 
     with smtplib.SMTP('smtp.gmail.com', 587) as server:
         # Start the TLS connection
@@ -53,12 +50,14 @@ while True:
     if cpu_percent > cpu_threshold:
         logging.error('CPU usage is over 80%')
         send_email('Error - CPU usage is over 80%')
+    
 
      #report an error if available disk space is lower than 20%
     disk_space = psutil.disk_usage('/')
     if disk_space.free < disk_threshold:
         logging.error('Available disk is less than 45GB')
         send_email('Error - Available disk is less than 45GB')
+       
 
     #report an error if available memory is less than 500MB
     memory = psutil.virtual_memory()
